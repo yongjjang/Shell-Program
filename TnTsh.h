e3e0e7045fff261b781e6ae83e01d150b63ecab5
이태용 <pastelhawk@gmail.com>
2019-11-23 19:05:46 +0900

    Simple Terminal Implement

--- TnTsh.h
+++ TnTsh.h
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void init_sh();
+void redirection();
+int getargs(char *cmd, char **argv);
+void eval();
+void run();
+
+char buf[256];
+char *argv[50];
+int narg;
+pid_t pid;
+
 
 
 
